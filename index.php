c8d09977fa846498271f08e10b03ad6e76b1ae05
RaulAgu <raulaguileraaparicio@hotmail.es>
2021-11-10 10:35:40 +0100

    subidaRaul

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "<form name=formRaul>";
+        echo "Nombre: <input type=text name=nombre>";
+        echo "Edad: <input type=number name=edad>";
+        echo "Fecha: <input type=date name=fecha>";
+        echo "Gustos: <input type=text name=gustos>";
+    echo "</form>";
+
+?>
